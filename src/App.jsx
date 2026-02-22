f7d9550e26de8ee9b53b7fc26948cec8d220adda
HarishB301 <harish33301@gmail.com>
2026-02-22 17:13:57 +0530

    this is my first react project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+// import MovieCard from './components/MovieCard'
+function App() {
+  
+
+  return (
+    <>
+    {/* <MovieCard ></MovieCard> */}
+    </>
+    
+  )
+}
+
+export default App
---
+++ src/components/MovieCard.jsx
@@ -0,0 +1,31 @@
+
+import { Heart } from 'lucide-react';
+const MovieCard = ({movie}) => {
+
+    const onFavouriteClick=()=>{
+        alert("fav Clicked..")
+
+    }
+
+  return (
+    <>
+    <div className="movie-card">
+        <div className="movie-poster">
+            <img src={movie.url} alt={movie.title} />
+            <div className="movie-overlay">
+                <button className="favourite-btn" onClick={onFavouriteClick}><Heart /></button>
+            </div>
+        </div>
+
+        <div className="movie-info">
+            <h3>{movie.title}</h3>
+            <p>{movie.release_date}</p>
+        </div>
+
+    </div>
+       
+    </>
+  )
+}
+
+export default MovieCard
